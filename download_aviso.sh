c38e85594c06f7921a47c3f88348f19001a3f783
liutongya <liutongya@sio.org.cn>
2021-05-11 16:54:42 +0800

    add download script

---
+++ download_aviso.sh
@@ -0,0 +1,1 @@
+python -m motuclient --motu https://my.cmems-du.eu/motu-web/Motu --service-id SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047-DGF --product-id dataset-duacs-rep-global-merged-allsat-phy-l4 --date-min "1993-01-01 00:00:00" --date-max "2020-06-03 00:00:00" --out-dir /data/data_pub/SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047-TDS/ --user xliu3 --pwd XiaohuiCMEMS2017
